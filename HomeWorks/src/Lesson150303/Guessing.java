98337708ad21ba0800810aae23e2e119e08e1749
Lydia Sokur <lydia.sokur@gmail.com>
2015-03-12 17:01:53 +0300

    Lesson150303 - Guessing - First part

--- HomeWorks/src/Lesson150303/Guessing.java
+++ HomeWorks/src/Lesson150303/Guessing.java
@@ -1,5 +1,18 @@
 package Lesson150303;
 
+import java.util.Scanner;
+
 public class Guessing {
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		boolean win = false;
+		int numberOfTries = 0;
+		int numberToGuess = input.nextInt();
+		
+		while (win == false){
+			
+		}
+	}
 
 }
