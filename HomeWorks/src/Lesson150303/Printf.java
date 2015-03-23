667622331a8d108af590fc3dfabe44252392add4
Lydia Sokur <lydia.sokur@gmail.com>
2015-03-23 16:41:38 +0400

    Homework 150303: Printf

--- HomeWorks/src/Lesson150303/Printf.java
+++ HomeWorks/src/Lesson150303/Printf.java
@@ -1,5 +1,18 @@
 package Lesson150303;
 
 public class Printf {
-
+	static int sum = 100;
+	static int bytes = 123456789;
+	static String string = "Сумма равна";
+	
+	public static void main(String[] args) {
+		System.out.printf("%s: %d\n", string, sum);
+		int mask = 0b1111111;
+		System.out.printf("%d --> %s %s %s %s\n",
+				bytes, 
+				Integer.toBinaryString(mask & bytes >>> 24),
+				Integer.toBinaryString(mask & bytes >>> 16),
+				Integer.toBinaryString(mask & bytes >>> 8),
+				Integer.toBinaryString(mask & bytes));
+	}
 }
