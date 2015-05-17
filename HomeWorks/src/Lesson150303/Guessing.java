f237bad985b675ee3873a2c30490d5db10c2c9e3
Lydia Sokur <lydia.sokur@gmail.com>
2015-05-18 02:16:59 +0400

    Homeworks Guessing and Deep Copy --> Guessing completed --> Deep Copy completed --> Tasks.txt deleted --> Added pretty header

--- HomeWorks/src/Lesson150303/Guessing.java
+++ HomeWorks/src/Lesson150303/Guessing.java
@@ -1,5 +1,43 @@
+/**
+Homework "Guessing game"
+Course by EPAM Systems
+"The complete development cycle using Java platform"
+
+Task:
+--> computer guessing the number in a range (1 - 100)
+--> number of guesses must be less than 7
+
+Written by Lydia Sokur, 2015
+ */
+
 package Lesson150303;
 
 public class Guessing {
 
+	public static void main(String[] args) {
+		java.util.Scanner scanner = new java.util.Scanner(System.in);
+		int max = 100;
+		int min = 1;
+		int number = 0, counter = 0;
+		String playersAnswer = "n";
+		
+		System.out.println("Let's play a game.");
+		System.out.println("Put forth a number between 1 and 100.");
+		System.out.println("Type:");
+		System.out.println("'y' if my guess is right,");
+		System.out.println("'h' if your number is higner,");
+		System.out.println("'l' if your number is less.");
+		
+		while (!playersAnswer.equals("y")) {
+			counter++;
+			number = min + (max - min)/2;
+			System.out.println("It is " + number + "?");
+			playersAnswer = scanner.next();
+			if (playersAnswer.equals("l")) max = max - (max - min)/2;
+			else if (playersAnswer.equals("h")) min = min + (max - min)/2;
+			else if (playersAnswer.equals("y")) break;
+		}
+		System.out.println("Hooray! Only " + counter + " moves!");
+		scanner.close();
+	}
 }
--- HomeWorks/src/lesson150305/DeepCopy.java
+++ HomeWorks/src/lesson150305/DeepCopy.java
@@ -1,5 +1,84 @@
+/**
+Homework "Deep copy"
+Course by EPAM Systems
+"The complete development cycle using Java platform"
+
+Task:
+Realize a deep copy of matrix of integers:
+--> with nested loop
+--> with System.arraycopy
+Add comparison of total time
+
+Written by Lydia Sokur, 2015
+*/
+
 package lesson150305;
 
-public class DeepCopy {
+import java.util.Arrays;
+import java.util.Random;
 
+public class DeepCopy {
+	
+	private static final int MAX = 1000;
+	private static final int ITERATIONS = 100;
+	
+	static long mineCopyTime = 0;
+	static long systemCopytime = 0;
+	
+	public static void main(String[] args) {
+		long start, stop;
+		
+		int[][] array;
+		int[][] systemArray = new int[MAX][MAX];
+		int[][] mineArray = new int[MAX][MAX];
+		
+		for(int i = 0; i < ITERATIONS; i++) {
+			array = generate();
+			
+			start = System.nanoTime();
+			mineArray = deepCopy(array);
+			stop = System.nanoTime();
+			mineCopyTime += stop - start;
+
+			start = System.nanoTime();
+			for (int j = 0; j < array.length; j++) {
+				System.arraycopy(array[j], 0, systemArray[j], 0, array[j].length);
+			}
+			stop = System.nanoTime();
+			systemCopytime += stop - start;			
+			
+			for (int j = 0; j < systemArray.length; j++) {
+				if (!Arrays.equals(systemArray[j], mineArray[j])) {
+					System.out.println("Copying is not right!");
+					break;
+				}				
+			}
+		}
+		System.out.println("System average time: " + systemCopytime/ITERATIONS);
+		System.out.println("Mine average time: " + mineCopyTime/ITERATIONS);
+		System.out.println("Ratio: " + (double)mineCopyTime/(double)systemCopytime);
+	}
+
+	private static int[][] deepCopy(int[][] array) {
+		int[][] newArray = new int[MAX][MAX];
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				newArray[i][j] = array[i][j];
+			}
+		}
+		return newArray;
+	}
+	
+	private static int[][] generate() {
+		int[][] data = new int[MAX][MAX];
+		
+		Random random = new Random();
+		
+		for (int i = 0; i < data.length; i++) {
+			for (int j = 0; j < data[i].length; j++) {
+				data[i][j] = random.nextInt(MAX);	
+			}
+		}		
+		return data;
+	}
 }
